c27d4753c6e88d1bdfa1f8087eea5f8167a605c9
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-04-20 09:26:32 +0300

    сщздали модуль входа

--- autorisation.js
+++ autorisation.js
@@ -1,4 +1,6 @@
 import { addTodo, deleteTodo, getTodos } from "./autorisationAPI.js";
+import { renderLoginComponent } from "./components/login-component.js";
+
 
 let tasks = [];
 let token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
@@ -17,36 +19,41 @@ const fetchTodosAndRender = () => {
 const renderApp = () => {
  const appEl = document.getElementById("app");
  if(!token){
-   const appHtml = `<h1>Список задач</h1>
-<div class="form">
- <h3 class="form-title">Форма входа</h3>
- <div class="form-row">
-   Логин
-   <input
-     type="text"
-     id="login-input"
-     class="input"
+//    const appHtml = `<h1>Список задач</h1>
+// <div class="form">
+//  <h3 class="form-title">Форма входа</h3>
+//  <div class="form-row">
+//    Логин
+//    <input
+//      type="text"
+//      id="login-input"
+//      class="input"
    
-   />
- </div>
- <div class="form-row">
-   Пароль
-   <input
-     type="text"
-     id="login-input"
-     class="input"
+//    />
+//  </div>
+//  <div class="form-row">
+//    Пароль
+//    <input
+//      type="text"
+//      id="login-input"
+//      class="input"
    
-   />
- </div>
- <br />
- <button class="button" id="login-button">Войти</button>
-</div>
-`;
-       appEl.innerHTML = appHtml;
-       document.getElementById('login-button').addEventListener('click',() =>{
-         token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
-     fetchTodosAndRender();
-       });
+//    />
+//  </div>
+//  <br />
+//  <button class="button" id="login-button">Войти</button>
+// </div>
+// `;
+//        appEl.innerHTML = appHtml;
+//        document.getElementById('login-button').addEventListener('click',() =>{
+//          token = "Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k";
+//      fetchTodosAndRender();
+//        });
+       renderLoginComponent({ appEl, setToken: (newToken) =>{
+    token = newToken;
+       },
+       fetchTodosAndRender
+    });
        return
 
  };

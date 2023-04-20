42ffff95caa8eb3f186d004a000b91ecc1f500f8
Romanbaranov1982 <romanbaranoff@rambler.ru>
2023-04-20 14:43:55 +0300

    добавляет форму регистрации

--- components/login-component.js
+++ components/login-component.js
@@ -1,14 +1,25 @@
-import { login } from "../autorisationAPI.js";
+import { loginUser } from "../autorisationAPI.js";
 
 export function renderLoginComponent ({
     appEl, 
     setToken,
     fetchTodosAndRender,
 }) {
-    const appHtml = `<h1>Список задач</h1>
+  let isLoginMode = true;
+  // функция изменения формы входа
+  const renderForm = ()=>{
+        const appHtml = `<h1>Список задач</h1>
     <div class="form">
-     <h3 class="form-title">Форма входа</h3>
+     <h3 class="form-title">Форма ${isLoginMode ? "входа" : "регистрации"}</h3>
      <div class="form-row">
+     ${isLoginMode ? "" : `Имя 
+     <input
+         type="text"
+         id="name-input"
+         class="input"       
+       />
+       <br></br>`}
+     
        Логин
        <input
          type="text"
@@ -20,28 +31,54 @@ export function renderLoginComponent ({
      <div class="form-row">
        Пароль
        <input
-         type="text"
-         id="login-input"
+         type="password"
+         id="password-input"
          class="input"
        
        />
      </div>
      <br />
-     <button class="button" id="login-button">Войти</button>
+     <button class="button" id="login-button">${isLoginMode ? "Войти" : "Зарегестрироваться"}</button>
+     <br></br>
+     <button class="button" id="toggle-button">Перейти к ${isLoginMode ? "регистрации" : "входу"}</button>
     </div>
     `;
            appEl.innerHTML = appHtml;
            document.getElementById('login-button').addEventListener('click',() =>{
+           const login = document.getElementById("login-input").value;
+           const password = document.getElementById("password-input").value;
+           if(!login){
+            alert('Введите логин');
+            return;
+           };
+           if(!password) {
+            alert("Введите пароль");
+            return;
+           }
+
          setToken("Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k");
-         login({
-          login: "admin",
-          password: "admin",
+         loginUser({
+          login: login,
+          password: password,
         })
         .then((user) =>{
-          console.log(user);
+         
           setToken(`Bearer ${user.user.token}`);
           fetchTodosAndRender();
         })
+        .catch((error) =>{
+           // выводим ошибку неправильного пароля или логина
+          alert(error.message);
+        })
          
            });
+           // обработчик клика на кнопку регистрации
+           document.getElementById("toggle-button").addEventListener('click', ()=>{
+           isLoginMode = !isLoginMode;
+                      renderForm();
+           })
+  }
+renderForm();
+
+
 }
